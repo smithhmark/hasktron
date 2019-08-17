9ebd069034d64d06dd567da381c78c2237f154d8
Mark Smith <smith.h.mark@gmail.com>
2019-08-17 11:57:06 -0700

    beginning basic NN ops

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,19 @@
 module Lib
-    ( someFunc
-    , someMsg
-    ) where
+  ( someFunc
+  , someMsg
+  , smoosh
+  ) where
 
 someMsg :: String
 someMsg = "someMsg"
 
 someFunc :: IO ()
 someFunc = putStrLn $ someMsg
+
+smoosh :: Float -> Float
+smoosh = undefined
+
+perceptron :: [Float] -> Float -> [Float] -> Float
+perceptron ws b as = b - s
+  where
+    s = sum $ zipWith (*) ws as
