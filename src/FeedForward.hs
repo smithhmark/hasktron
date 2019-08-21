4e2dec1cb5f4dacb72fbc484ebec78d3a5ac375f
Mark Smith <smith.h.mark@gmail.com>
2019-08-21 12:54:29 -0700

    adding feed forward evalution as a function

--- src/FeedForward.hs
+++ src/FeedForward.hs
@@ -1,5 +1,6 @@
 module FeedForward
   ( nextLayer
+  , feedForward
   ) where
 
 import           Neuron
@@ -9,3 +10,12 @@ nextLayer :: Neuron -> Activations -> [Weights] -> [Bias] -> Activations
 nextLayer pfn as wss bs = foldr op [] $ zip wss bs
   where
     op (ws, b) acc = pfn ws b as : acc
+
+feedForward' :: Neuron -> Activations -> [Layer] -> Activations
+feedForward' n as [] = as
+feedForward' n as (Layer {weights = w, biases = b}:ls) = feedForward' n next ls
+  where
+    next = nextLayer n as w b
+
+feedForward :: [Layer] -> [Double] -> [Double]
+feedForward ls is = feedForward' perceptron is ls
--- test/FeedForwardSpec.hs
+++ test/FeedForwardSpec.hs
@@ -7,6 +7,7 @@ import           Test.QuickCheck
 
 import           FeedForward
 import           Neuron
+import           NNTypes
 
 main :: IO ()
 main = hspec spec
@@ -19,3 +20,17 @@ spec = do
       let wss = [[1.0, 2.0], [3.0, 4.0]]
       let bs = [5.0, 6.0]
       nextLayer (freePerceptron) as wss bs `shouldBe` [0.0, 5.0]
+  describe "feedForward" $ do
+    it "processes multi layer network correctly" $ do
+      let net =
+            [ Layer
+                { weights = [[1, 2, 3], [2, 3, 1], [3, 1, 2]]
+                , biases = [10.0, 12.0, 14.0]
+                }
+            , Layer {weights = [[1, 2, 3], [3, 2, 1]], biases = [2, 3]}
+            ]
+      let inputs = [3.0, 2.0, 1.0]
+      let expts = [0.6832918574, 0.5575090141]
+      let rcvd = feedForward net inputs
+      let normalize ds = map (truncate . (* 1000000)) ds
+      normalize rcvd `shouldBe` normalize expts
