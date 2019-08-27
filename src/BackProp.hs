05ad14aa270f49e53d823f1ab02c8b427b2b664d
Mark Smith <smith.h.mark@gmail.com>
2019-08-27 11:38:00 -0700

    gardening and docs

--- src/BackProp.hs
+++ src/BackProp.hs
@@ -24,36 +24,50 @@ learningZipperFromNetwork = fromList
 learnFromExample :: Network -> Input -> Targets -> NetworkDiff
 learnFromExample = undefined
 
+-- | forwardPhase computes the feed forward phase of network
 forwardPhase :: NetZip -> Input -> NetZip
 forwardPhase nz is = forwardPhase' nz2
   where
     nz1 = start nz
     nz2 = procLayerD nz1 op
-    op = flip fwdEvalLayer is
+    op = flip evalLayerFwd is
 
+-- | forwardPhase' is the recursive worker fuction that transforms a layer
+-- and moves to the next
 forwardPhase' :: NetZip -> NetZip
 forwardPhase' z@NetZip {deeper = []} = z
 forwardPhase' z@NetZip {shallower = s:ss, deeper = d:ds} =
   forwardPhase' $ z {shallower = s2 : s : ss, deeper = ds}
   where
-    s2 = fwdEvalLayer d $ acts s
+    s2 = evalLayerFwd d $ acts s
 
-fwdEvalLayer :: LLayer -> NNT.Activations -> LLayer
-fwdEvalLayer layer@LLayer {weights = wss, biases = bs} is =
+-- | evalLayerFwd does the work off transforming a layer based on prior
+-- activiations
+evalLayerFwd :: LLayer -- ^ the current layer being processed
+             -> NNT.Activations -- ^ the activations from the previous layer
+             -> LLayer -- ^ the input layer transformed with the results
+evalLayerFwd layer@LLayer {weights = wss, biases = bs} is =
   layer {outs = foldr op [] $ zip wss bs}
   where
     op (ws, b) acc = (z, logistic z) : acc
       where
         z = freePerceptron ws b is
 
-dCost :: NNT.Activation -> Double -> Double
+-- | the derivative of the cost with respect to a neuron's output
+dCost :: NNT.Activation -- ^ a neuron's output activation
+      -> Double -- ^ the target value
+      -> Double -- ^ d(C)/d(o_i)
 dCost o x = o - x
 
+-- | computes the corrections for the bias and the weights of a neuron
 outputNeuronGradient ::
-     NNT.Activation -> Double -> NNT.Activations -> (NNT.Bias, NNT.Weights)
-outputNeuronGradient o e ps = (deltaB, deltaWs)
+     NNT.Activation -- ^ output of the neuron
+     -> Double -- ^ the target output
+     -> NNT.Activations -- ^ activations from prior level
+     -> (NNT.Bias, NNT.Weights)
+outputNeuronGradient o t ps = (deltaB, deltaWs)
   where
-    dC_do = dCost o e
+    dC_do = dCost o t
     do_dz = logisticPrime o
     deltaB = dC_do * do_dz
     deltaWs = map (\p -> deltaB * p) ps
